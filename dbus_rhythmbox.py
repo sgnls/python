19262d8947640e3b43fcedcdc7183dbd0abfb8be
Matt Hill <global@sgnls.net>
2016-03-23 18:26:09 +0000

    Commit

--- dbus_rhythmbox.py
+++ dbus_rhythmbox.py
@@ -5,7 +5,6 @@ session_bus = dbus.SessionBus()
 
 rb_obj = session_bus.get_object(
     'org.gnome.Rhythmbox', '/org/gnome/Rhythmbox/Player')
-
 player = dbus.Interface(rb_obj, 'org.gnome.Rhythmbox.Player')
 
 print player.getPlayingUri()
